ae8fc8fecb55d0988c1c8088334ca164d482fee4
gossdouglas <gossdouglas@yahoo.com>
2020-03-18 17:48:29 -0500

    tbl_eqpmt_type_mgmt
    added support for tbl_eqpmt_type_mgmt

--- Models/allpax_sale_miner.Context.cs
+++ Models/allpax_sale_miner.Context.cs
@@ -23,5 +23,6 @@
         public virtual DbSet<tbl_eqpmt_type> tbl_eqpmt_type { get; set; }
         public virtual DbSet<tbl_eqpmt_kits_avlbl> tbl_eqpmt_kits_avlbl { get; set; }
         public virtual DbSet<tbl_customer_eqpmt> tbl_customer_eqpmt { get; set; }
+        public virtual DbSet<tbl_eqpmt_type_mgmt> tbl_eqpmt_type_mgmt { get; set; }
     }
 }
