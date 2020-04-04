de77aa8031c2407f593267bca8cfa4d1a1f2fe31
thegibster49 <31262488+thegibster49@users.noreply.github.com>
2020-04-04 13:04:07 -0500

    event ID dropdown

--- Controllers/CustomerEventController.cs
+++ Controllers/CustomerEventController.cs
@@ -21,6 +21,7 @@ namespace allpax_sale_miner.Controllers
             List<tbl_customer_event> custEvent = entities.tbl_customer_event.ToList();
 
             ViewBag.customerCode = new SelectList(db.tbl_customer, "customerCode", "customerCode");
+            ViewBag.eventID = new SelectList(db.tbl_event_type, "eventID", "eventID");
 
             return View(custEvent.ToList());
         }
