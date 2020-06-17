80c0d026a45f9e67ac7c4ff5c86b64327656c4d6
gossdouglas <gossdouglas@yahoo.com>
2020-06-17 00:19:16 -0500

    set up support for equipment with kits avlbl but not installed.

---
+++ Controllers/machinesW_kitsAvlbl_BnotInstalledController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using allpax_sale_miner.ViewModels;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace allpax_sale_miner.Controllers
+{
+    public class machinesW_kitsAvlbl_BnotInstalledController : Controller
+    {
+        // GET: machinesW_kitsAvlbl
+        public ActionResult Index()
+        {
+            List<machinesW_kitsAvlbl_BnotInstalled> mWkaBni = new List<machinesW_kitsAvlbl_BnotInstalled>();
+            string mainconn = ConfigurationManager.ConnectionStrings["allpax_sale_minerEntities"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "SELECT cmps411.tbl_customer_eqpmt.customerCode, cmps411.tbl_customer_eqpmt.eqpmtType, cmps411.tbl_customer_eqpmt.model, cmps411.tbl_customer_eqpmt.machineID," +
+
+                "cmps411.tbl_eqpmt_kits_current.machineID, cmps411.tbl_eqpmt_kits_current.kitID," +
+
+                "cmps411.tbl_eqpmt_kits_avlbl.model, cmps411.tbl_eqpmt_kits_avlbl.kitID " +
+
+                "FROM cmps411.tbl_customer_eqpmt " +
+
+                "LEFT JOIN cmps411.tbl_eqpmt_kits_current ON cmps411.tbl_customer_eqpmt.machineID = tbl_eqpmt_kits_current.machineID " +
+                "LEFT JOIN cmps411.tbl_eqpmt_kits_avlbl ON cmps411.tbl_customer_eqpmt.model = tbl_eqpmt_kits_avlbl.model " +
+
+                "WHERE cmps411.tbl_eqpmt_kits_current.machineID is null AND cmps411.tbl_eqpmt_kits_current.kitID is null";
+
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            foreach(DataRow dr in dt.Rows)
+            {
+                machinesW_kitsAvlbl_BnotInstalled mWkitsAvlblBnotInstalled = new machinesW_kitsAvlbl_BnotInstalled();
+
+                mWkitsAvlblBnotInstalled.customerCode = dr["customerCode"].ToString();
+                mWkitsAvlblBnotInstalled.eqpmtType = dr["eqpmtType"].ToString();
+                mWkitsAvlblBnotInstalled.model = dr["model"].ToString();
+                mWkitsAvlblBnotInstalled.machineID = dr["machineID"].ToString();
+                mWkitsAvlblBnotInstalled.kitsCurrent_machineID = dr["machineID"].ToString();
+                mWkitsAvlblBnotInstalled.kitsCurrent_kitID = dr["kitID"].ToString();
+                mWkitsAvlblBnotInstalled.kitsAvlbl_model = dr["model"].ToString();
+                mWkitsAvlblBnotInstalled.kitsAvlbl_kitID = dr["kitID"].ToString();
+
+                mWkaBni.Add(mWkitsAvlblBnotInstalled);
+            }
+            return View(mWkaBni);
+        }
+    }
+}
---
+++ ViewModels/machinesW_kitsAvlbl_BnotInstalled.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace allpax_sale_miner.ViewModels
+{
+    public class machinesW_kitsAvlbl_BnotInstalled
+    {
+        public string customerCode { get; set; }
+        public string eqpmtType { get; set; }
+        public string model { get; set; }
+        public string machineID { get; set; }
+        public string kitsCurrent_machineID { get; set; }
+        public string kitsCurrent_kitID { get; set; }
+        public string kitsAvlbl_model { get; set; }
+        public string kitsAvlbl_kitID { get; set; }
+    }
+}
