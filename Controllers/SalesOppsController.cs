de4f65f39409fd35573a5752cee9dd6950f004f4
gossdouglas <gossdouglas@yahoo.com>
2020-06-16 20:55:21 -0500

    not so great.  trying something different.

--- Controllers/SalesOppsController.cs
+++ Controllers/SalesOppsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Dynamic;
 using System.Linq;
 using System.Net;
 using System.Web; 
@@ -14,28 +15,39 @@ namespace allpax_sale_miner.Controllers
     {
      
         // GET: CustomerMgmt
-        public ActionResult Index(salesOppsModel som)
+        public ActionResult Index()
         {
               allpax_sale_minerEntities db = new allpax_sale_minerEntities();
-        //List<tbl_customer_eqpmt> custEqpmt = db.tbl_customer_eqpmt.ToList();
-        //List<tbl_eqpmt_kits_current> kitsCurrent = db.tbl_eqpmt_kits_current.ToList();
-        //List<tbl_eqpmt_kits_avlbl> kitsAvlbl = db.tbl_eqpmt_kits_avlbl.ToList();
 
-        string query = "SELECT cmps411.tbl_customer_eqpmt.customerCode, cmps411.tbl_customer_eqpmt.eqpmtType, " +
-                "cmps411.tbl_customer_eqpmt.model, cmps411.tbl_customer_eqpmt.machineID, cmps411.tbl_eqpmt_kits_current.machineID, " +
-                "cmps411.tbl_eqpmt_kits_current.kitID, cmps411.tbl_eqpmt_kits_avlbl.model, cmps411.tbl_eqpmt_kits_avlbl.kitID " +
-                "FROM cmps411.tbl_customer_eqpmt " +
-                "LEFT JOIN cmps411.tbl_eqpmt_kits_current ON cmps411.tbl_customer_eqpmt.machineID = tbl_eqpmt_kits_current.machineID " +
-                "LEFT JOIN cmps411.tbl_eqpmt_kits_avlbl ON cmps411.tbl_customer_eqpmt.model = tbl_eqpmt_kits_avlbl.model " +
-                "WHERE cmps411.tbl_eqpmt_kits_current.machineID is null AND cmps411.tbl_eqpmt_kits_current.kitID is null";
+            // begin https://www.youtube.com/watch?v=EXimdPaGkDw this works
+            List<tbl_customer_eqpmt> custEqpmt = db.tbl_customer_eqpmt.ToList();
+            List<tbl_eqpmt_kits_current> kitsCurrent = db.tbl_eqpmt_kits_current.ToList();
+            List<tbl_eqpmt_kits_avlbl> kitsAvlbl = db.tbl_eqpmt_kits_avlbl.ToList();         
 
-        var data = db.Database.SqlQuery<salesOppsModel>(query).ToList();
+            //var multipleTable = from ce in custEqpmt
+            //                    join kc in kitsCurrent on ce.machineID equals kc.machineID
+            //                    select new salesOppsModel { tbl_customer_eqpmt = ce, tbl_eqpmt_kits_current = kc };
+            //return View(multipleTable);
+            //end https://www.youtube.com/watch?v=EXimdPaGkDw this works
 
+            //string query = "SELECT cmps411.tbl_customer_eqpmt.customerCode, cmps411.tbl_customer_eqpmt.eqpmtType, " +
+            //        "cmps411.tbl_customer_eqpmt.model, cmps411.tbl_customer_eqpmt.machineID, cmps411.tbl_eqpmt_kits_current.machineID, " +
+            //        "cmps411.tbl_eqpmt_kits_current.kitID, cmps411.tbl_eqpmt_kits_avlbl.model, cmps411.tbl_eqpmt_kits_avlbl.kitID " +
+            //        "FROM cmps411.tbl_customer_eqpmt " +
+            //        "LEFT JOIN cmps411.tbl_eqpmt_kits_current ON cmps411.tbl_customer_eqpmt.machineID = tbl_eqpmt_kits_current.machineID " +
+            //        "LEFT JOIN cmps411.tbl_eqpmt_kits_avlbl ON cmps411.tbl_customer_eqpmt.model = tbl_eqpmt_kits_avlbl.model " +
+            //        "WHERE cmps411.tbl_eqpmt_kits_current.machineID is null AND cmps411.tbl_eqpmt_kits_current.kitID is null";
 
-            return View(data);
             
+            var query = "SELECT * FROM cmps411.tbl_customer_eqpmt";           
+            var data = db.Database.SqlQuery<salesOppsModel>(query).ToList();
+ 
+            return View(data);
+            //return View(multipleTable);
+            //return View(tables);
+
         }
-                                
-      
+
+
     }
 }
--- Models/salesOppsModel.cs
+++ Models/salesOppsModel.cs
@@ -7,9 +7,13 @@ namespace allpax_sale_miner.Models
 {
     public class salesOppsModel
     {
+        //public IEnumerable<tbl_customer_eqpmt> tbl_customer_eqpmt { get; set; }
+        //public IEnumerable<tbl_eqpmt_kits_current> tbl_eqpmt_kits_current { get; set; }
+        //public IEnumerable<tbl_eqpmt_kits_avlbl> tbl_eqpmt_kits_avlbl { get; set; }
+
         public tbl_customer_eqpmt tbl_customer_eqpmt { get; set; }
-        public tbl_eqpmt_kits_current tbl_eqpmt_kits_current { get; set; }
-        public tbl_eqpmt_kits_avlbl tbl_eqpmt_kits_avlbl { get; set; }
+        //public tbl_eqpmt_kits_current tbl_eqpmt_kits_current { get; set; }
+        //public tbl_eqpmt_kits_avlbl tbl_eqpmt_kits_avlbl { get; set; }
 
     }
-}
+} 
