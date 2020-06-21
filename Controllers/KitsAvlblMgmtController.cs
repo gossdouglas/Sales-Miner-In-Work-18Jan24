198a252d22f46d27f1bd695dd7b8517be94cb0be
gossdouglas <gossdouglas@yahoo.com>
2020-06-21 16:20:30 -0500

    Update KitsAvlblMgmtController.cs

--- Controllers/KitsAvlblMgmtController.cs
+++ Controllers/KitsAvlblMgmtController.cs
@@ -16,9 +16,6 @@ namespace allpax_sale_miner.Controllers
 
         public ActionResult Index()
         {
-            //allpax_sale_minerEntities entities = new allpax_sale_minerEntities();
-            //List<tbl_eqpmt_kits_avlbl> kitAvlbl = entities.tbl_eqpmt_kits_avlbl.ToList();
-
             ViewBag.eqpmtType = new SelectList(db.tbl_eqpmt_type.OrderBy(x => x.eqpmtType), "eqpmtType", "eqpmtType");
             ViewBag.model = new SelectList(db.tbl_eqpmt_type.OrderBy(x => x.model), "model", "model");
             ViewBag.kitID = new SelectList(db.tbl_kit.OrderBy(x => x.kitID), "kitID", "kitID");
@@ -32,16 +29,6 @@ namespace allpax_sale_miner.Controllers
         [HttpPost]
         public ActionResult AddAvlblKit(tbl_eqpmt_kits_avlbl avlblKitAdd)
         {
-            //using (allpax_sale_minerEntities entities = new allpax_sale_minerEntities())
-            //{
-            //    entities.tbl_eqpmt_kits_avlbl.Add(new tbl_eqpmt_kits_avlbl()
-            //    {
-            //        eqpmtType = avlblKitAdd.eqpmtType,
-            //        model = avlblKitAdd.model,
-            //        kitID = avlblKitAdd.kitID
-            //    });
-            //    entities.SaveChanges();
-            //}
             db.Database.ExecuteSqlCommand("Insert into cmps411.tbl_eqpmt_kits_avlbl Values({0},{1},{2})",
                 avlblKitAdd.eqpmtType, avlblKitAdd.model, avlblKitAdd.kitID);
             return RedirectToAction("Index");
@@ -49,24 +36,12 @@ namespace allpax_sale_miner.Controllers
 
         public ActionResult DeleteAvlblKit(tbl_eqpmt_kits_avlbl avlblKitDelete)
         {
-            //tbl_eqpmt_kits_avlbl tbl_eqpmt_kits_avlbl = db.tbl_eqpmt_kits_avlbl.Find(avlblKitDelete.id);
-            //db.tbl_eqpmt_kits_avlbl.Remove(tbl_eqpmt_kits_avlbl);
-            //db.SaveChanges();
             db.Database.ExecuteSqlCommand("DELETE FROM cmps411.tbl_eqpmt_kits_avlbl WHERE id=({0})", avlblKitDelete.id);
             return RedirectToAction("Index");
         }
 
         public ActionResult UpdateAvlblKit(tbl_eqpmt_kits_avlbl avlblKitUpdate)
         {
-            //using (allpax_sale_minerEntities entities = new allpax_sale_minerEntities())
-            //{
-            //    tbl_eqpmt_kits_avlbl updatedAvlblKit = (from c in entities.tbl_eqpmt_kits_avlbl
-            //                                       where c.id == avlblKitUpdate.id
-            //                                       select c).FirstOrDefault();
-            //    updatedAvlblKit.eqpmtType = avlblKitUpdate.eqpmtType;
-            //    updatedAvlblKit.kitID = avlblKitUpdate.kitID;
-            //    entities.SaveChanges();
-            //}
             db.Database.ExecuteSqlCommand("UPDATE cmps411.tbl_eqpmt_kits_avlbl SET eqpmtType={0},model={1},kitID={2} WHERE id={3}",
                 avlblKitUpdate.eqpmtType, avlblKitUpdate.model, avlblKitUpdate.kitID, avlblKitUpdate.id);
             return new EmptyResult();
