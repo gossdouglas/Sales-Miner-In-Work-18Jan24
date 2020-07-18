124882d9e8b6e0d192868e9a3a84f943466081b1
gossdouglas <gossdouglas@yahoo.com>
2020-07-18 14:05:52 -0500

    can add 5 kits through the ui now.
    delete works, also.  need to work on editing a record.

--- Controllers/KitMgmtController.cs
+++ Controllers/KitMgmtController.cs
@@ -25,8 +25,13 @@ namespace allpax_sale_miner.Controllers
         [HttpPost]
         public ActionResult AddKit(tbl_kit kitAdd)
         {
-            db.Database.ExecuteSqlCommand("Insert into cmps411.tbl_kit Values({0},{1},{2},{3},{4},{5},{6},{7},{8})", 
-                kitAdd.kitID, kitAdd.description, kitAdd.filePath, kitAdd.kitItem_1, kitAdd.kitItem_2, kitAdd.kitItem_3, kitAdd.kitItem_4, kitAdd.kitItem_5);
+            db.Database.ExecuteSqlCommand("Insert into cmps411.tbl_kit Values({0},{1},{2},{3},{4},{5},{6},{7}, " +
+                "'', '', '', '', '', '', '', '', '', '', '', '', '', '', '' )", 
+                kitAdd.kitID, kitAdd.description, kitAdd.filePath, kitAdd.kitItem_1, kitAdd.kitItem_2, kitAdd.kitItem_3, kitAdd.kitItem_4, kitAdd.kitItem_5, 
+                kitAdd.kitItem_6, kitAdd.kitItem_7, kitAdd.kitItem_8, kitAdd.kitItem_9, kitAdd.kitItem_10, kitAdd.kitItem_11, kitAdd.kitItem_12, 
+                kitAdd.kitItem_13, kitAdd.kitItem_14, kitAdd.kitItem_15, kitAdd.kitItem_16, kitAdd.kitItem_17, kitAdd.kitItem_18, kitAdd.kitItem_19, kitAdd.kitItem_20);
+
+
             return new EmptyResult();
         }
 
